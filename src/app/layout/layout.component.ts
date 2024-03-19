534cc317e71840eff720074eff2031903a828a30
fakhar <fakharyaseen25@gmail.com>
2024-03-19 22:02:07 +0500

    fixed mobile responsive

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,33 +1,46 @@
-import { animate, state, style, transition, trigger } from "@angular/animations";
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
 import { Component, HostListener, OnInit } from "@angular/core";
 import { MatIconModule } from "@angular/material/icon";
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
 import { Router, RouterModule } from "@angular/router";
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from "@angular/material/dialog";
 import { AddEditProductsComponent } from "../add.edit.products/add.edit.products.component";
 import { RequestService } from "../services/requestService";
 import { ConfirmationDialogComponent } from "../confirmation-dialog/confirmation-dialog.component";
 @Component({
   selector: "app-layout",
   standalone: true,
-  imports: [MatIconModule, HeaderComponent, FooterComponent, MatDialogModule, RouterModule],
+  imports: [
+    MatIconModule,
+    HeaderComponent,
+    FooterComponent,
+    MatDialogModule,
+    RouterModule,
+  ],
   templateUrl: "./layout.component.html",
   styleUrl: "./layout.component.scss",
 })
-
 export class LayoutComponent implements OnInit {
-  admin: any
+  admin: any;
   products: any[] = [];
   loading: boolean = false;
 
-  constructor(private router: Router,
+  constructor(
+    private router: Router,
     public dialog: MatDialog,
-    private requestService: RequestService) {
-    this.admin = JSON.parse(localStorage.getItem("admin") || '[]');
+    private requestService: RequestService
+  ) {
+    this.admin = JSON.parse(localStorage.getItem("admin") || "[]");
     window.scroll({
       top: 0,
-      left: 0
+      left: 0,
     });
   }
 
@@ -37,11 +50,12 @@ export class LayoutComponent implements OnInit {
 
   addProduct(item?: any) {
     const dialogRef = this.dialog.open(AddEditProductsComponent, {
+      // height: "900px",
       disableClose: true,
-      data: item
+      data: item,
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result === "save") {
         this.getAllProducts();
       }
@@ -50,36 +64,42 @@ export class LayoutComponent implements OnInit {
 
   getAllProducts() {
     this.loading = true;
-    this.requestService.getAllProducts().subscribe(res => {
-      if (res) {
+    this.requestService.getAllProducts().subscribe(
+      (res) => {
+        if (res) {
+          this.loading = false;
+          this.products = res.data;
+        }
+      },
+      (err) => {
         this.loading = false;
-        this.products = res.data;
       }
-    }, (err) => {
-      this.loading = false;
-    })
+    );
   }
 
   deleteConfirmation(id: any) {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
-      disableClose: true
+      disableClose: true,
     });
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result === "yes") {
-        this.deleteProducts(id)
+        this.deleteProducts(id);
       }
     });
   }
 
   deleteProducts(id: any) {
-    this.loading =true;
-    this.requestService.removeProduct(id).subscribe(res => {
-      if (res) {
+    this.loading = true;
+    this.requestService.removeProduct(id).subscribe(
+      (res) => {
+        if (res) {
+          this.loading = false;
+          this.getAllProducts();
+        }
+      },
+      (err) => {
         this.loading = false;
-        this.getAllProducts();
       }
-    }, (err) => {
-      this.loading = false;
-    })
+    );
   }
 }
